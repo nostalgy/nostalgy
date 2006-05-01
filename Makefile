0265b0ecd852fe580096e88a5f75ea4e8d0c58b2
frisch <frisch@56b81dcf-5a2f-0410-9db0-014be2e416ff>
2006-05-01 19:40:47 +0000

    Initial revision

---
+++ Makefile
@@ -0,0 +1,5 @@
+pack:
+	./build.sh
+
+clean:
+	rm -f nostalgy.jar nostalgy.xpi
---
+++ config_build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Build config for build.sh
+APP_NAME=nostalgy
+CHROME_PROVIDERS="content locale"
+CLEAN_UP=0
+ROOT_FILES=
+ROOT_DIRS="defaults"
+BEFORE_BUILD=
+AFTER_BUILD=

54a9875d397a7a0df9e9017aa0f9aca8877dcf70
frisch <frisch@56b81dcf-5a2f-0410-9db0-014be2e416ff>
2006-11-06 18:01:24 +0000

    *** empty log message ***

---
+++ content/composer.js
@@ -0,0 +1,38 @@
+function gEBI(s) { return document.getElementById(s); }
+
+var nostalgy_old_awRecipientKeyPress = 0;
+
+function nostalgy_awRecipientKeyPress(event, element) {
+  if (event.charCode == 32 && element.selectionStart == 2) {
+    var id = element.id;
+    if (id.match(/addressCol2#/)) {
+    var select = gEBI(id.replace(/addressCol2#/,"addressCol1#"));
+    var v = element.value;
+    var u = v.replace(/ >> .*/, "");
+    var f = v.substr(0,2);
+    if (f == "to" || f == "cc") {
+	select.value = "addr_" + f;
+	element.value = "";
+	setTimeout(function(){
+	  element.value = u.substr(2,u.length - 2);
+ 	  element.selectionStart = 0; element.selectionEnd = 0;
+        }, 0);
+	event.preventBubble();
+	event.stopPropagation();
+	return;
+    }
+    }
+  }
+  nostalgy_old_awRecipientKeyPress(event, element);
+}
+
+function onNostalgyLoad(){
+  var tbox = gEBI("addressingWidget");
+  nostalgy_old_awRecipientKeyPress = window.awRecipientKeyPress;
+  window.awRecipientKeyPress = nostalgy_awRecipientKeyPress;
+  alert("UUUX");
+/*  window.addEventListener("keypress", onNostalgyKeyPress, true);
+  alert("T"); */
+}
+
+window.addEventListener("load", onNostalgyLoad, false);

7159813377341bfcc2bc74c54ee210d224fc4131
frisch <frisch@56b81dcf-5a2f-0410-9db0-014be2e416ff>
2007-03-07 17:13:32 +0000

    allow Bcc:

--- trunk/content/composer.js
+++ trunk/content/composer.js
@@ -3,24 +3,23 @@ function gEBI(s) { return document.getElementById(s); }
 var nostalgy_old_awRecipientKeyPress = 0;
 
 function nostalgy_awRecipientKeyPress(event, element) {
-  if (event.charCode == 32 && element.selectionStart == 2) {
-    var id = element.id;
-    if (id.match(/addressCol2#/)) {
-    var select = gEBI(id.replace(/addressCol2#/,"addressCol1#"));
-    var v = element.value;
-    var u = v.replace(/ >> .*/, "");
-    var f = v.substr(0,2);
-    if (f == "to" || f == "cc") {
-	select.value = "addr_" + f;
-	element.value = "";
-	setTimeout(function(){
-	  element.value = u.substr(2,u.length - 2);
- 	  element.selectionStart = 0; element.selectionEnd = 0;
-        }, 0);
-	event.stopPropagation();
-	return;
-    }
-    }
+  var id = element.id;
+  if (id.match(/addressCol2#/)) {
+  var select = gEBI(id.replace(/addressCol2#/,"addressCol1#"));
+  var v = element.value;
+  var u = v.replace(/ >> .*/, "");
+  var i = element.selectionStart;
+  var f = v.substr(0,i);
+  if (event.charCode == 32 && (f == "to" || f == "cc" || f == "bcc")) {
+    select.value = "addr_" + f;
+    element.value = "";
+    setTimeout(function(){
+      element.value = u.substr(i,u.length - i);
+      element.selectionStart = 0; element.selectionEnd = 0;
+    }, 0);
+    event.stopPropagation();
+    return;
+  }
   }
   nostalgy_old_awRecipientKeyPress(event, element);
 }

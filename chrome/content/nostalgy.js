da64cc118388b3114a698ebd505dd140493ca209
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2020-03-11 18:20:37 +0000

    Use unicode arrow and tooltip for folder name

--- chrome/content/nostalgy.js
+++ chrome/content/nostalgy.js
@@ -287,8 +287,9 @@ function NostalgyHide(restore) {
 function NostalgyDefLabel() {
  nostalgy_gsuggest_folder = NostalgySuggest();
  if (nostalgy_gsuggest_folder) {
-   nostalgy_label.label =
-       nostalgy_default_label + " [+Shift: => " + NostalgyFolderName(nostalgy_gsuggest_folder) + "]";
+   var nostalgy_folder_name = NostalgyFolderName(nostalgy_gsuggest_folder);
+   nostalgy_label.setAttribute("tooltiptext", nostalgy_folder_name);
+   nostalgy_label.label = nostalgy_default_label + " [+Shift ⇒ " + nostalgy_folder_name + "]";
  } else {
    nostalgy_label.label = nostalgy_default_label;
  }
